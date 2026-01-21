f1565f410a7dccac98b6b18135a47ab5e4a84b48
yatsukki <kervenc35@gmail.com>
2026-01-21 02:51:16 -0500

    Struggling with question 5

---
+++ task 1/task 1 folder/js/script 5.js
@@ -0,0 +1,79 @@
+"use strict";
+var white = "#ffffff"
+var ellipseAlpha = 100
+var radius = 40
+var counter = 0
+const gscale = 50
+
+
+let orangeButton = {
+    x: 30,
+    y: 30,
+    default: "#ff5125ff",
+    hover: "#ffffffff",
+    size: 40
+}
+
+let redButton = {
+    x: 80,
+    y: 30,
+    color: "#ff0263ff",
+    size: 40
+}
+//creates button
+function displaySquare(color,x,y,size) {
+    noStroke();
+    fill(color);
+    square(x,y,size)
+
+}
+
+function setup (){
+    console.log("I'm so hungry");
+    createCanvas(500, 500);
+    background(gscale);
+    
+    
+}
+
+function draw(){
+    mouseHover();
+    background(gscale);
+    displaySquare(orangeButton.default,orangeButton.x,orangeButton.y,orangeButton.size)
+    displaySquare(redButton.color,redButton.x,redButton.y,redButton.size)
+    
+    if (checkCollisionWithSquare()){
+        createEllipse();
+    }
+    
+
+}
+//&& mouseY > orangeButton.y && mouseY < orangeButton.y+orangeButton.size
+//mouse hover function
+function mouseHover(){
+    let hovercolor = "#ffffffff"
+
+    if (checkCollisionWithSquare()) {
+        hovercolor = orangeButton.hover
+        orangeButton.default = orangeButton.hover
+        console.log("hover is working")
+    }
+           
+}
+//counter +1 when mouse is clicked on collision with square
+function mouseClicked(){
+    if (checkCollisionWithSquare()) {
+        counter += 1;
+        
+        console.log("click is working")
+    }
+}
+
+function checkCollisionWithSquare(){
+    return mouseX > orangeButton.x && mouseX < orangeButton.x+orangeButton.size && mouseY > orangeButton.y && mouseY < orangeButton.y+orangeButton.size
+}
+
+function createEllipse(){
+    fill("#ffffff")
+    ellipse(width/2, height/2, radius,radius)
+}
