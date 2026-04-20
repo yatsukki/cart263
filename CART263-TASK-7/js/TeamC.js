504fd0187336f4c8a8773b00ecd685790dcd67c3
yatsukki <kervenc35@gmail.com>
2026-04-20 06:56:23 -0400

    attempting task 7

--- CART263-TASK-7/js/TeamC.js
+++ CART263-TASK-7/js/TeamC.js
@@ -28,8 +28,17 @@ export class PlanetC {
         const mesh2 = new THREE.Mesh(geometryC1,materialC1)
 
         //position
-        mesh.position.set(0, 0, -20);
-        mesh2.position.set(0, 4, -5);
+        // mesh.position.set(0, 0, -20);
+        // mesh2.position.set(0, 3.4, -2);
+
+            //add light
+        const ambientLight = new THREE.AmbientLight()
+            scene.add(ambientLight)
+        //intensity and color
+        
+        ambientLight.color = new THREE.Color("#ffffff")
+        ambientLight.intensity = 0.5;
+        
         //TODO: Use castShadow and receiveShadow on the mesh and all future ones so they can cast and receive shadows.
        
        
@@ -38,12 +47,13 @@ export class PlanetC {
 
         //TODO: Add the planet mesh to the planet group.
 
-        scene.add(mesh, )
+        this.group.add(mesh)
 
         
         //STEP 2: 
         //TODO: Add from 1 to 3 orbiting moons to the planet group. 
-        mesh.add(mesh2)
+
+        // mesh.add(mesh2)
 
         //TODO: The moons should rotate around the planet just like the planet group rotates around the Sun.
 
@@ -52,33 +62,53 @@ export class PlanetC {
 
             //initate GLTF
         
+            this.loadModels()
+            //TODO: Make sure to rotate the models so they are oriented correctly relative to the surface of the planet.
+            
+            //STEP 4:
+            //TODO: Use raycasting in the click() method below to detect clicks on the models, and make an animation happen when a model is clicked.
+            //TODO: Use your imagination and creativity!
+            this.scene.add(this.group);
+            
+        }
+        async loadModels(){
+
         const gltfLoader = new GLTFLoader();
 
          //null cuz theyre not loaded yet
         let gltfUfo = null;
 
+
+
         try {
-           gltfUfo = await gltfLoader.loadAsync('model/ufo/ufo.gltf')
+           gltfUfo = await gltfLoader.loadAsync('models/ufo/ufo.gltf')
+           console.log(gltfUfo)
+           this.model = gltfUfo.scene.children[0]
+           this.group.add(this.model)
+
+           //position for model
+        //    model.position.y = 10
+        //    model.position.z = 10
+        //    model.position.x = 10
+        
         }
 
         catch (error){
         console.log(error.message)
         }
 
-        //TODO: Make sure to rotate the models so they are oriented correctly relative to the surface of the planet.
-        
-        //STEP 4:
-        //TODO: Use raycasting in the click() method below to detect clicks on the models, and make an animation happen when a model is clicked.
-        //TODO: Use your imagination and creativity!
-        this.scene.add(this.group);
+        }
 
-    }
-    
+
+   
     update(delta) {
         // Orbit around sun
         this.angle += this.orbitSpeed * delta * 30;
         this.group.position.x = Math.cos(this.angle) * this.orbitRadius;
         this.group.position.z = Math.sin(this.angle) * this.orbitRadius;
+
+        this.model.position.z = Math.sin(this.angle) * (this.orbitRadius/4);
+        this.model.position.x = Math.sin(this.angle) * (this.orbitRadius/4);
         
         // Rotate planet
         this.group.rotation.y += delta*0.5;
@@ -88,6 +118,26 @@ export class PlanetC {
 
     click(mouse, scene, camera) {
         //TODO: Do the raycasting here.
+        const raycaster = new THREE.Raycaster()
+
+        
+        //ray will start somewhere on left of the spheresx
+        //set direction only (has length ==1)
+        rayDirection.normalize()
+        console.log(rayDirection.length())
+        raycaster.set(rayOrigin, rayDirection) //raycaster has been oriented
+
+        //cast a ray - check intersection with obj1, obj2 and obj 3 
+        const intersects = raycaster.intersectObjects([mesh, mesh2, this.model])
+        console.log(intersects)
+
+
+        const mouse = new THREE.Vector2();
+        window.addEventListener("mousemove", function(event) {
+        mouse.x = (event.clientX / sizes.width) * 2 - 1; //map to between -1,1
+        mouse.y = -(event.clientY / sizes.height) * 2 + 1; //map to between -1,1
+        //console.log(mouse);
+  });
     }
 }
 
