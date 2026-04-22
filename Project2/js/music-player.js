f597675778ce03f21ccd9d7e0f66a2be9ad6afab
yatsukki <kervenc35@gmail.com>
2026-04-21 20:17:23 -0400

    Created json file for music info
    created arrays for the songs info

---
+++ Project2/js/music-player.js
@@ -0,0 +1,70 @@
+const songs = [
+    {   cover:"../media/music/covers/song1",
+        name: "When the stardust hits",
+        trackNo: "1.",
+        duration: "3:31",
+        audio: "../media/music/song1"
+    },
+    {   cover:"../media/music/covers/song2",
+        name: "Boss of me",
+        trackNo: "2.",
+        duration: "2:58",
+        audio: "../media/music/song2"
+    },
+
+    {   cover:"../media/music/covers/song3",
+        name: "Luxury",
+        trackNo: "3.",
+        duration: "3:26",
+        audio: "../media/music/song3"
+    },
+
+    {   cover:"../media/music/covers/song4",
+        name: "glitter ✩‧₊˚",
+        trackNo: "4.",
+        duration: "3:36",
+        audio: "../media/music/song4"
+    },
+
+    {   cover:"../media/music/covers/song5",
+        name: "Heaven",
+        trackNo: "5.",
+        duration: "2:31",
+        audio: "../media/music/song5"
+    },
+
+    {   cover:"../media/music/covers/covers/song6",
+        name: "Romance",
+        trackNo: "6.",
+        duration: "3:31",
+        audio: "../media/music/song6"
+    },
+
+    {   cover:"../media/music/covers/song7",
+        name: "Sunny Duet",
+        trackNo: "7.",
+        duration: "3:34",
+        audio: "../media/music/song7"
+    },
+
+    {   cover:"../media/music/covers/song8",
+        name: "Girl like me",
+        trackNo: "8.",
+        duration: "2:26",
+        audio: "../media/music/song8"
+    },
+
+    {   cover:"../media/music/covers/song9",
+        name: "GOODBYENOKUBAY - SAYONARA!!",
+        trackNo: "9.",
+        duration: "2:46",
+        audio: "../media/music/song9"
+    },
+
+    {   cover:"../media/music/covers/song10",
+        name: "hyvät herrat – Rework",
+        trackNo: "10.",
+        duration: "2:09",
+        audio: "../media/music/song10"
+    },
+]
